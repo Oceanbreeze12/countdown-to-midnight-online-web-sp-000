8baf4b31ac0373dda50c8f2d8e7548d393cd461f
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-03-11 22:03:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,4 @@
-def countdown
+def countdown(num)
   while num > 0
   puts "#{num} SECOND(S)!"
   num -= 1
@@ -7,7 +7,7 @@ end
 
   
 
-def countdown_with_sleep
+def countdown_with_sleep(num)
  while  num > 0
 puts "#{num} SECOND(S)!"
   sleep(1)
